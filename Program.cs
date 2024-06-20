1d4169a6d1d3e99b2db50207c4a322da0b18bb8c
mkonain <mkonain@folio3.com>
2024-06-20 12:20:14 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Please enter number: ");
+var num = Console.ReadLine();
+
+var result = CalculateFactroial(Convert.ToInt32(num));
+
+Console.Write(string.Format("factorial: {0}" ,result));
+
+long CalculateFactroial(int number)
+{
+    if (number > 1)
+        return CalculateFactroial(number - 1) * number;
+
+    return number;
+}
+
